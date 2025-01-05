fa95248dd5149a03d3b492e2e4d6301c61bd0fb5
Wajackoya <141861669+Wajackoya@users.noreply.github.com>
2025-01-05 19:21:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -52,7 +52,7 @@ module.exports = {
     SESSION: (process.env.SESSION || process.env.SESSION_ID || 'Raganork~0079b8a18d4f0381bc5887d2cdbef519:424d45626a69486d4a71383d').trim() || '',
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
     RG: process.env.RG || '919074309534-1632403322@g.us,120363116963909366@g.us',
-    BOT_INFO: process.env.BOT_INFO || 'ミ★ 𝘙𝘈𝘎𝘈𝘕𝘖𝘙𝘒 ★彡;░⡷▁🅦🅐🅙🅐🅒🅚🅢▁⢾░;254102510747;https://i.imgur.com/P7ziVhr.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
+    BOT_INFO: process.env.BOT_INFO || 'ミ★ 𝘙𝘈𝘎𝘈𝘕𝘖𝘙𝘒 ★彡;░⡷▁🅦🅐🅙🅐🅒🅚🅢▁⢾░;254102510747;https://imgur.com/Net5lhZ;https://chat.whatsapp.com/LbjZf963upIBoCU4zkfhgD',
     RBG_KEY: process.env.RBG_KEY || '',
     ALLOWED: process.env.ALLOWED || '91,94,2',
     NOT_ALLOWED: process.env.ALLOWED || '91,94,212',
